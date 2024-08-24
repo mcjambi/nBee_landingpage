473facb8aee02fa09f4f8f29450f62f15aa7208b
Jam Viet <mcjambi@gmail.com>
2024-08-24 09:26:40 +0700

    Fixed product collection

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './media/css/App.scss';
 import reportWebVitals from './reportWebVitals';
 import { HelmetProvider } from 'react-helmet-async';
 import '@shopify/polaris/build/esm/styles.css';
-import 'media/css/RichTextEditor.scss';
+import 'media/css/RichTextContent.scss';
 import viTranslations from '@shopify/polaris/locales/vi.json';
 import { AppProvider } from '@shopify/polaris';
 
